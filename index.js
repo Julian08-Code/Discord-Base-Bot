5da0e19c74fad9a2b4e810be9762cf7a78ccb27a
Julian Jarabo <julianjar08@gmail.com>
2020-05-09 14:24:24 +0200

    El token era invalido pero sigo mirando

--- index.js
+++ index.js
@@ -3,13 +3,14 @@ const { prefix, token } = require('./config.json');
 const client = new Discord.Client();
 
 client.once('ready', () => {
-	console.log('Ready!');
+	console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.on('message', message => {
-	if (message.content === '!ping') {
+	if (message.content === `${prefix}ping`) {
 		message.channel.send('Pong.');
 	}
 });
+
 
 client.login( token );
